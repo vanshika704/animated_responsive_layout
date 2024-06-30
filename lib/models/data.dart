08a72370eceb5757a2d1c84e1667de6841a743d0
vanshika704 <sharmavanshi704@gmail.com>
2024-07-01 00:06:05 +0530

    explanation

--- lib/models/data.dart
+++ lib/models/data.dart
@@ -1,7 +1,11 @@
 import 'models.dart';
 
+// isme , jo humne model bnaya h , uska data diya h ,
 final User user_0 = User(
-    name: const Name(first: 'Me', last: ''),
+    //  user ko call lgai , usko name diya
+    name: const Name(
+        first: 'Me',
+        last: ''), // name ko call lgai , usme last name or first name diya
     avatarUrl: 'assets/avatar_1.png',
     lastActive: DateTime.now());
 final User user_1 = User(
@@ -23,8 +27,9 @@ final User user_4 = User(
 
 final List<Email> emails = [
   Email(
-    sender: user_1,
-    recipients: [],
+    // yha email ki list h
+    sender: user_1, // sender ka name user_1 h
+    recipients: [], // recepients ka array aayega
     subject: '豆花鱼',
     content: '最近忙吗？昨晚我去了你最爱的那家饭馆，点了他们的特色豆花鱼，吃着吃着就想你了。',
   ),
@@ -52,6 +57,7 @@ final List<Email> emails = [
 ];
 
 final List<Email> replies = [
+  // ye replies hain , jo aayi hui mails ko humne diye , same cheej h , class bnai , usme data diya
   Email(
     sender: user_2,
     recipients: [
@@ -73,3 +79,10 @@ final List<Email> replies = [
         "Yes! I forgot about that place! I'm definitely up for taking a risk this week and handing control over to this mysterious noodle chef. I wonder what happens if you have allergies though? Lucky none of us have any otherwise I'd be a bit concerned.\n\nThis is going to be great. See you all at the usual time?",
   ),
 ];
+// poore code me  seekhne wali  baat ye h ki data kaise diya jaata h function ko call lgake
+// agr dekha jaaaye , toh structure kya h ?
+// user ko call lgai jaati h , or jo humne required parameters set kiye the , unko data diya jaa rha h
+// aur humne jo data diya h , usko humne list me diya h ,
+// aur humne jo list diya h , usko humne function ko pass kiya h
+// aur function ne usko use kiya h , or usko reply me diya h ,
+// aur humne jo reply diya h , usko humne list me diya h
